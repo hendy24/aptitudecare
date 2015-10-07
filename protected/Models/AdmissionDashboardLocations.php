b13f0e2fff44752bacebabd20585f22ce5343e10
Kemish W. Hendershot <kemish@aptitudeit.net>
2015-10-07 11:32:23 -0700

    Moved naming the database into a construct method.
    Signed-off-by: Kemish W. Hendershot <kemish@aptitudeit.net>

--- protected/Models/AdmissionDashboardLocations.php
+++ protected/Models/AdmissionDashboardLocations.php
@@ -4,5 +4,11 @@ class AdmissionDashboardLocations extends AppModel {
 
 	protected $prefix = false;
 	protected $table = "x_site_user_link_facility";
+
+	public function __construct() {
+		$this->dbname = db()->dbname2;
+	}
+
+
 	
 }
