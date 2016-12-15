9b81ad4455204e5eeab44b4554bbf9e704ff7fcf
Kemish W. Hendershot <kemish@aptitudeit.net>
2016-12-15 13:25:55 -0700

    Fixed snack label query... again.
    Signed-off-by: Kemish W. Hendershot <kemish@aptitudeit.net>

--- modules/Dietary/Models/Snack.php
+++ modules/Dietary/Models/Snack.php
@@ -22,7 +22,7 @@ class Snack extends Dietary {
     CONCAT(patient.last_name, ', ', patient.first_name) AS patient_name, 
     (SELECT GROUP_CONCAT(diet_order.name, ', ') FROM dietary_patient_diet_order AS patient_diet_order INNER JOIN dietary_diet_order diet_order ON diet_order.id = patient_diet_order.diet_order_id WHERE patient_diet_order.patient_id = patient.id GROUP BY patient_diet_order.patient_id) AS diet,
     snack.name,
-    (SELECT GROUP_CONCAT(allergy.name, ' ') FROM dietary_patient_food_info AS patient_allergy INNER JOIN dietary_allergy allergy ON allergy.id=patient_allergy.food_id WHERE patient_allergy.patient_id=patient.id) AS allergy, 
+    (SELECT GROUP_CONCAT(allergy.name, ' ') FROM dietary_patient_food_info AS patient_allergy INNER JOIN dietary_allergy allergy ON allergy.id=patient_allergy.food_id WHERE patient_allergy.patient_id=patient.id AND patient_allergy.allergy = 1) AS allergy, 
     patient_snack.time,
     (SELECT GROUP_CONCAT(t.name, ' ') FROM {$patient_texture->tableName()} AS pt INNER JOIN {$texture->tableName()} t ON t.id = pt.texture_id WHERE pt.patient_id = patient.id) AS texture
 FROM dietary_snack AS snack 
