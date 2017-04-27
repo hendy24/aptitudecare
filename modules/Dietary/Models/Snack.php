3fdb02fd2a80b4269194abfc829d73f9adeebf26
kemish <kemish@tbcidaho.com>
2017-04-27 09:32:38 -0600

    Removed patients from the snack list who have a discharge date but do not have a Discharged status
    Signed-off-by: kemish <kemish@tbcidaho.com>

--- modules/Dietary/Models/Snack.php
+++ modules/Dietary/Models/Snack.php
@@ -20,6 +20,11 @@ class Snack extends Dietary {
 
         $snack_time = array('am', 'pm', 'bedtime');
         $params[":location_id"] = $location_id;
+
+        // the time is set for midnight because they are printing out tickets. Could be a problem if someone is discharged
+        // from a room and someone else is admitted to that room. May need to change mysql_date() to mysql_datetime() to get
+        // the current time 
+        $params[":now"] = mysql_date() . " 23:59:59";
         $result = array();
 
         foreach ($snack_time as $time) {
@@ -36,7 +41,7 @@ class Snack extends Dietary {
                 INNER JOIN dietary_patient_snack patient_snack ON patient_snack.snack_id = snack.id 
                 INNER JOIN ac_patient patient ON patient.id = patient_snack.patient_id INNER JOIN admit_schedule sch ON sch.patient_id = patient.id 
                 INNER JOIN admit_room room ON room.id = sch.room_id 
-            WHERE sch.location_id = :location_id AND sch.status='Approved' AND patient_snack.time = :snack_time
+            WHERE sch.location_id = :location_id AND sch.status='Approved' AND (sch.datetime_discharge IS NULL OR sch.datetime_discharge >= :now) AND patient_snack.time = :snack_time
             GROUP BY room.number, patient_snack.time 
             ORDER BY room.number, patient_snack.time";
             $result[] = $this->fetchAll($sql, $params);
