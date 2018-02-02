2c317dd9ba02585f1bee39d72409cd4ad9515059
kemish <kemish.hendershot@gmail.com>
2018-02-02 11:26:16 -0700

    Uncommented section to allow switching between modules
    Signed-off-by: kemish <kemish.hendershot@gmail.com>

--- protected/Controllers/MainPageController.php
+++ protected/Controllers/MainPageController.php
@@ -80,7 +80,6 @@ class MainPageController extends MainController {
 			// if there is module set then set it to the session
 			session()->setModule($this->module);
 		}
-
 		// Set the content from the controller and cooresponding view
 		$this->setContent();
 
@@ -88,7 +87,7 @@ class MainPageController extends MainController {
 		if (auth()->isLoggedIn()) {
 			if ($this->action == "admission_login") {
 				// this is a hack to allow switching between modules until the admission module is rebuilt.
-				//$this->module = "HomeHealth";
+				$this->module = "HomeHealth";
 			} else {
 				$this->module = session()->getModule();
 			}
